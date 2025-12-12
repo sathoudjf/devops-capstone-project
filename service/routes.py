1126179492dc564e955fbb73ea482c738c23549f
sathoudjf <sathoudjf@gmail.com>
2025-12-11 21:51:25 -0500

    Added badge for GitHub Actions

--- service/routes.py
+++ service/routes.py
@@ -22,6 +22,7 @@ def health():
 # GET INDEX
 ######################################################################
 
+
 @app.route("/")
 def index():
     """Root URL response"""
@@ -37,6 +38,7 @@ def index():
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
@@ -63,6 +65,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -81,6 +84,7 @@ def list_accounts():
 
 # ... place you code here to READ an account ...
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -98,6 +102,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -118,6 +123,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
